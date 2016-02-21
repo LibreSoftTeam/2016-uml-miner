e6b9198950f380c956d6ca65519a1eb6d9eb780f
mghfdez <mafesan.nsn@gmail.com>
2016-02-21 23:01:01 +0100

    Modified: now checks files ina different way (now filenames are owner_id:repo_id.json). To do that, it creates two dictionaries with that info from the analysed csv

--- hits2urls.py
+++ hits2urls.py
@@ -6,14 +6,25 @@
 # https://raw.githubusercontent.com/bartsitek/ci.boilerplate/master/application/assets/img/social/designfloat_16.png
 
 import json
+import os
 import os.path
+import csv
+import sys
+from collections import namedtuple
+
+if len(sys.argv) != 2:
+    print "Usage: $python hits2urls.py <projects csv>"
 
 start = "https://raw.githubusercontent.com/"
+init_path = os.path.abspath(os.curdir)
+ProjectRecord = namedtuple('ProjectRecord', 'id, url, owner_id, name, descriptor, language, created_at, forked_from, deleted, updated_at')
+owners_dict = {}
+projects_dict = {}
 
 def obtain_branch(username, repo):
     """
     """
-    filepath = "cpu03/default/" + username + ":" + repo + ".json"
+    filepath = init_path + "/default/" + username + ":" + repo + ".json"
     if os.path.isfile(filepath):
         with open(filepath) as data_file:
             data = json.load(data_file)
@@ -25,6 +36,15 @@ def obtain_branch(username, repo):
 
     return "master"
 
+with open(sys.argv[1], "r") as csvfile: # CSV file name here!
+    for contents in csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC):
+        contents[0] = int(contents[0])
+        contents[2] = int(contents[2])
+        row = ProjectRecord(*contents)
+        owner_name = row.url.split('/')[4]
+        projects_dict[row.name] = row.id
+        owners_dict[owner_name] = row.owner_id
+
 with open('hits.txt', 'r') as file:
     linelist = file.readlines()
 
@@ -33,13 +53,16 @@ for line in linelist:
         continue
 #    print "*", line[:-1]
     try:
+        print line
         path, blob = line[:-1].split(" https://api.github.com/")
     except ValueError:
         continue
     blobList = blob.split('/')
     username = blobList[1]
     repo = blobList[2]
-    branch = obtain_branch(username, repo)
+    username_id = str(owners_dict[username])
+    repo_id = str(projects_dict[repo])
+    branch = obtain_branch(username_id, repo_id)
     if not branch:
         continue
     total = start + username + "/" + repo + "/" + branch + "/" + path
