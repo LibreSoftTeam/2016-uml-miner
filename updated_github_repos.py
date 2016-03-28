45de5a308aeaeea7cb5eb1bd286290287e17d0c8
mghfdez <mafesan.nsn@gmail.com>
2016-03-29 01:50:53 +0200

    Adaptation to new output csv's with UML files

--- updated_github_repos.py
+++ updated_github_repos.py
@@ -9,33 +9,30 @@ command = "cvsanaly2 -u operator -p operator -d uml_github"
 already = []
 
 current = os.getcwd()
+repos_dir = "uml_repos"
 line = 0
-with open('../updated.csv', 'r') as csvfile:
+with open('updated.csv', 'r') as csvfile:
     for updatedCSV in csv.reader(csvfile):
-        if line < 721:
-            line+=1
-            continue
-        if 'https://git.eclipse.org/' in updatedCSV[0]:
-            continue
-        print(updatedCSV[0])
-        user, repo, _ = updatedCSV[0].strip().split('/')
-        branch = updatedCSV[1].split('/')[5]
-        repo_url = ''.join(['https://github.com/', user, '/', repo])
-        if (user, repo, branch) not in already:
-            if not os.path.exists(user):
-                os.makedirs(user)
-            os.chdir(user)
-            cloning = ' '.join(['git clone', repo_url, '--branch', branch])
-            print(cloning)
-            os.system(cloning)
-            already.append((user, repo, branch))
-
-            # CVSAnalY in action!
-            print("Running CVSAnaly on " + repo)
-            try:
-                os.chdir(repo)
-            except OSError:
-                print("ERROR: Could not find directory", repo)
-                continue
-            os.system(command)
-            os.chdir(current)
+        if updatedCSV[3] == "UML":
+            list_url = updatedCSV[2].split('/')
+            user = list_url[3]
+            repo = list_url[4]
+            branch = list_url[5]
+            repo_url = ''.join(['https://github.com/', user, '/', repo])
+            if (user, repo, branch) not in already:
+                if not os.path.exists(user):
+                    os.makedirs(user)
+                os.chdir(user)
+                cloning = ' '.join(['git clone', repo_url, '--branch', branch])
+                print(cloning)
+                os.system(cloning)
+                already.append((user, repo, branch))
+                # CVSAnalY in action!
+                print("Running CVSAnaly on " + repo)
+                try:
+                    os.chdir(repo)
+                except OSError:
+                    print("ERROR: Could not find directory", repo)
+                    continue
+                os.system(command)
+                os.chdir(current)
